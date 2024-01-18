6ea593939c47b5525f432b65489d199d2c98e1ea
Yago <yagovr.rocha@gmail.com>
2024-01-18 15:50:46 -0300

    Update input

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
-document.querySelector('#createGrid').addEventListener('click', function() {
     let container = document.querySelector("#gridContainer");
-    let input = prompt("Select the size of the grid: ");
-    let size = parseInt(input);
-    
-    if (input === null || isNaN(input) || input < 1 || input > 100) {
-        alert("Invalid input or canceled. Please enter a number between 1 and 100.");
-        return;
+    let gridSize = document.querySelector("#gridSize");
+    let gridRange = document.querySelector("#gridRange");
+
+    function change() {
+        let value = gridRange.value;
+        gridSize.innerHTML = `${value} x ${value}`;
+        
+        createGrid();
     }
 
-    container.innerHTML = '';
-    
-    for (let i = 0; i < size * size; i++){
-        const gridSquare = document.createElement("div");
-        gridSquare.classList.add("grid-square");
-        container.appendChild(gridSquare);
-        gridSquare.style.width = `calc(100% / ${size})`;
-        gridSquare.style.paddingBottom = `calc(100% / ${size})`;
-        gridSquare.addEventListener('click', function(){
-            gridSquare.style.backgroundColor = 'black';
-        })
-    
+    function createGrid(){
+        let size = parseInt(gridRange.value);
+
+        container.innerHTML = '';
+        
+        for (let i = 0; i < size * size; i++){
+            let gridSquare = document.createElement("div");
+            gridSquare.classList.add("grid-square");
+            container.appendChild(gridSquare);
+            gridSquare.style.width = `calc(100% / ${size})`;
+            gridSquare.style.paddingBottom = `calc(100% / ${size})`;
+            gridSquare.addEventListener('click', function(){
+                gridSquare.style.backgroundColor = 'black';
+            })
+        
+        }
     }
 
-    
-    
-})
+    gridRange.addEventListener("input", change);
