0598480026b87601aee989594f03c5c94bdea508
Yago <yagovr.rocha@gmail.com>
2024-01-19 17:41:08 -0300

    The grids has a max size

--- main.js
+++ main.js
@@ -1,30 +1,37 @@
-    let container = document.querySelector("#gridContainer");
-    let gridSize = document.querySelector("#gridSize");
-    let gridRange = document.querySelector("#gridRange");
+let container = document.querySelector("#gridContainer");
+let gridSize = document.querySelector("#gridSize");
+let gridRange = document.querySelector("#gridRange");
 
-    function change() {
-        let value = gridRange.value;
-        gridSize.innerHTML = `${value} x ${value}`;
-        
-        createGrid();
+function change() {
+    let value = gridRange.value;
+    gridSize.innerHTML = `${value} x ${value}`;
+    
+    createGrid();
+}
+
+function createGrid(){
+    let size = parseInt(gridRange.value);
+    let containerWidth = Math.min(window.innerWidth, window.innerHeight) * 0.8;
+
+    container.innerHTML = '';
+    
+    for (let i = 0; i < size * size; i++){
+        let gridSquare = document.createElement("div");
+        gridSquare.classList.add("grid-square");
+        container.appendChild(gridSquare);
+
+        let squareSize = containerWidth / size;
+        gridSquare.style.width = `${squareSize}px`;
+        gridSquare.style.height = `${squareSize}px`;
+
+        gridSquare.addEventListener('click', function(){
+            gridSquare.style.backgroundColor = 'black';
+        })
     }
 
-    function createGrid(){
-        let size = parseInt(gridRange.value);
+    container.style.maxWidth = `${containerWidth}px`;
+    container.style.maxHeight = `${containerWidth}px`;
+}
 
-        container.innerHTML = '';
-        
-        for (let i = 0; i < size * size; i++){
-            let gridSquare = document.createElement("div");
-            gridSquare.classList.add("grid-square");
-            container.appendChild(gridSquare);
-            gridSquare.style.width = `calc(100% / ${size})`;
-            gridSquare.style.paddingBottom = `calc(100% / ${size})`;
-            gridSquare.addEventListener('click', function(){
-                gridSquare.style.backgroundColor = 'black';
-            })
-        
-        }
-    }
-
-    gridRange.addEventListener("input", change);
+gridRange.addEventListener("input", change);
+createGrid();
